bfc87e4a788f55e3e4e55791923f24d0832a3f35
djoni <djoni0204@gmail.com>
2024-11-01 12:16:17 +0000

    Better Variable Names

--- DNA.py
+++ DNA.py
@@ -1,29 +1,33 @@
 from math import sin
-from time import sleep  
+from time import sleep
 
-''' This is a meaningless comment. '''
-
-sign = '*'
-width = 50
-occurence = 1 #keeps count of how many times it should make a full line
-i = 1 
-
+# Configuration parameters
+character = '*'
+wave_amplitude = 50
+pass_count = 1  # Tracks the number of complete cycles
+angle_increment = 0.05
+angle = 1  # Initial angle for the sine wave
 
 while True:
-    sleep(0.1) # how fast it should move
-    i += 0.05 
-    occurence += 1
-    sinus = sin(i) * width
+    sleep(0.1)  # Controls the animation speed
+    angle += angle_increment
+    pass_count += 1
+    sinus_value = sin(angle) * wave_amplitude
     
-    if (sinus < 0): #makes sure it never gets in the negative
-        sinus = sinus * -1
+    # Ensure sinus_value is positive for consistent spacing
+    if sinus_value < 0:
+        sinus_value = abs(sinus_value)
     
-    emptySpace = sinus - 2 * 2
+    # Calculate spacing between DNA strands
+    inner_space = sinus_value - 4
     
-    if occurence % 2 == 0: #makes a line every 2nd pass through
-        line = sign * int(sinus)
+    # Alternate between full line and spaced pattern
+    if pass_count % 2 == 0:
+        dna_line = character * int(sinus_value)
     else:
-        line = sign * 2 + ' ' * int(emptySpace) + sign * 2
+        dna_line = character * 2 + ' ' * int(inner_space) + character * 2
 
-    x = line.center(100, ' ') #centers the DNA
-    print(x)
+    # Center the DNA line within a fixed width
+    centered_line = dna_line.center(100, ' ')
+    print(centered_line)
+
